9257a14065e8514c0a6a46441977fdff29f63f79
awebgame <32238655+awebgame@users.noreply.github.com>
2017-09-27 15:09:07 +0200

    Final version

--- js/main.js
+++ js/main.js
@@ -1,210 +1,155 @@
-var f_name = "";
-var m_name = "";
+var apiKey = "6pxjCQsVyNXqE3GTbgzFglwsNayg85fwu6vvPA5kZ872jsMwmS";
 
-var f_score = 0;
-var m_score = 0;
+var tumblrEndpoint = "https://api.tumblr.com/v2/blog/best-hot-gifs.tumblr.com/";
 
-var counter = 1;
-var current_player = 1;
+var gifApi = tumblrEndpoint + "posts/photo?api_key=" + apiKey;
 
-var max = 5;
+var infoApi = tumblrEndpoint + "info" + "?api_key=" + apiKey;
 
-var t_option = 0;
-var a_option = 0;
-var b_option = 0;
+var gifs;
 
-var guid = "";
-var token = "";
+var f_score;
+var m_score;
+var f_name;
+var m_name;
 
-var api_url = "https://oauth.reddit.com/r/NSFW_GIF/search.json?restrict_sr=on&include_over_18=on&sort=relevance&t=all&limit=50";
+var rounds;
 
-f_tags = ["dildo", "vibrator", "blowjob", "finger", "footjob", "ass", "69", "cowgirl", "boobs", "suck+cock", "spread", "tit+fuck", "doggy", "position", "anal", "couple"];
-m_tags = ["lick+pussy", "lick+ass", "doggy", "fuck+pussy", "69", "spank", "finger+ass", "position", "anal", "couple"];
+var f_accept;
+var m_accept;
+var f_skip;
+var m_skip;
+var count;
 
-function guidGenerator() {
-  var S4 = function() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-  };
-  return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4());
-}
-
-function start_game() {
+function initGame() {
   f_score = 0;
   m_score = 0;
-  counter = 1;
-  current_player = 1;
-  guid = "";
+  f_name = "";
+  m_name = "";
+  rounds = 0;
+  count = 0;
 
-  max = document.getElementById('max_score').value;
+  if(document.getElementById('female_input').value != '' && document.getElementById('male_input').value != '' && (document.getElementById('rounds_input').value > 4 && document.getElementById('rounds_input').value < 21)) {
+    var totalPosts = 0;
+    var offset = 0;
 
+    f_name = capitalizeFirstLetter(document.getElementById('female_input').value);
+    m_name = capitalizeFirstLetter(document.getElementById('male_input').value);
 
-  f_name = capitalizeFirstLetter(document.getElementById('female_input').value);
-  m_name = capitalizeFirstLetter(document.getElementById('male_input').value);
+    rounds = document.getElementById('rounds_input').value;
 
-  if(f_name === "" || m_name === "") {
-    alert("Please enter the player names");
-  } else {
-
-    guid = guidGenerator();
+    document.getElementById('f_player_name').innerText = f_name;
+    document.getElementById('m_player_name').innerText = m_name;
 
     $.ajax({
-      type: "POST",
-      url: "https://www.reddit.com/api/v1/access_token",
+      url: infoApi,
       dataType: 'json',
-      async: false,
-      headers: {
-        "Authorization": "Basic " + btoa("9pOsP_Q4bmSSIA:i3pQtUJhQnVz6zvz7-4jCesikvQ")
-      },
-      data: {grant_type: 'https://oauth.reddit.com/grants/installed_client', device_id: guid},
-      success: function(data){
-        token = data['access_token'];
+      success: function(res) {
+        totalPosts = res["response"]["blog"].posts - rounds;
+
+        offset = Math.floor((Math.random() * totalPosts));
+
+        $.ajax({
+          url: gifApi + "&limit=" + rounds + "&offset=" + offset,
+          dataType: 'json',
+          success: function(res) {
+            gifs = res["response"]["posts"];
+
+            document.getElementById('game_start').style.display = 'none';
+
+            document.getElementById('game_container').style.display = 'block';
+            loadGif();
+          }
+        });
       }
-    });
-/*
-    t_option = document.getElementById('t_option').checked;
-    a_option = document.getElementById('a_option').checked;
-    b_option = document.getElementById('b_option').checked;
-
-    if(a_option) {
-      couple_tags.push(a_tags);
-    }
-*/
-
-    document.getElementById('game_start').style.display = 'none';
-
-    document.getElementById('f_score_name').innerHTML = f_name;
-    document.getElementById('m_score_name').innerHTML = m_name;
-    document.getElementById('game_container').style.display = 'block';
-    load_next();
-  }
-}
-
-function score_color() {
-  if(f_score > m_score) {
-    document.getElementById('f_score').classList.remove('text-error');
-    document.getElementById('f_score').classList.add('text-success');
-    document.getElementById('m_score').classList.remove('text-success');
-    document.getElementById('m_score').classList.add('text-error');
-  } else if(m_score < f_score) {
-    document.getElementById('m_score').classList.remove('text-error');
-    document.getElementById('m_score').classList.add('text-success');
-    document.getElementById('f_score').classList.remove('text-success');
-    document.getElementById('f_score').classList.add('text-error');
+    });  
   } else {
-    document.getElementById('f_score').classList.remove('text-error');
-    document.getElementById('m_score').classList.remove('text-error');
-    document.getElementById('f_score').classList.add('text-success');
-    document.getElementById('m_score').classList.add('text-success');
+    alert("Please enter the details");
   }
 }
 
-function load_next() { 
-  if(f_score == max || m_score == max) {
-    end_game();
-  }
+function loadGif() {
+  f_accept = 0;
+  m_accept = 0;
+  f_skip = 0;
+  m_skip = 0;
 
-  current_player = !current_player;
-  
-  if(current_player == 0) {
-    document.getElementById('player_name').innerHTML = f_name;
-   } else {
-    document.getElementById('player_name').innerHTML = m_name;
-   } 
-
-   load_gif();
-}
-
-function update_score(player) {
-  if(current_player == 0) {
-    f_score++;
-    document.getElementById('f_score_value').innerHTML = f_score;
+  if(count != rounds) {
+    document.getElementById('round_info').innerHTML = 'Round ' + count;
+    document.getElementById('current_gif').innerHTML = '<img class="gif_img" src="' + gifs[count]["photos"][0]["original_size"].url + '" />';
+    count++;
   } else {
-    m_score++;
-    document.getElementById('m_score_value').innerHTML = m_score;
+    endGame();
+  }
+} 
+
+function accept(player) {
+  if(player == 0) {
+    f_accept = 1;
+  } else {
+    m_accept = 1;
+  }
+  checkStatus();
+}
+
+function skip(player) {
+  if(player == 0) {
+    f_skip = 1;
+  } else {
+    m_skip = 1;
   }  
-
-  load_next();
-  score_color();
+  checkStatus();
 }
 
-function load_gif() {
-  current_tag = "";  
-
-  if(current_player == 0) {
-    f_random = Math.floor((Math.random() * f_tags.length));
-    current_tag = f_tags[f_random]; 
-    url = api_url + "&q=" + current_tag;
-  } else {
-
-    m_random = Math.floor((Math.random() * m_tags.length));
-    current_tag = m_tags[m_random];
-    url = api_url + "&q=" + current_tag;
-  }
-
-
-  console.log(current_tag);
-
-  $.ajax({
-    type: "GET",
-    url: url,
-    dataType: 'json',
-    async: false,
-    headers: {
-      "Authorization": "Bearer " + token
-    },
-    success: function(data){
-      n_random = Math.floor((Math.random() * data["data"]["children"].length));
-      applyGif(data["data"]["children"][n_random]["data"]);
-    }
-  });
-}
-
-function applyGif(gif) {
-  var elem = document.getElementById('current_gif');
-  url = "";
-  if(gif.domain == "gfycat.com") {
-    if($.isEmptyObject(gif.media_embed)) {
-      load_gif();
-    } else {
-      decoded = $('<div/>').html(gif["media_embed"].content).text();
-      elem.innerHTML = decoded;
-    }
-  }
-  else if(gif.domain == "i.minus.com") {
-    load_gif();
-  }
-  else if(gif.domain == "i.imgur.com") {
-    url = gif.url.substr(0, gif.url.lastIndexOf(".")) + ".gif";
-    elem.innerHTML = '<img class="gif_img" src="' + url + '" />';
+function checkStatus() {
+  if(f_skip && m_accept) {
+    m_score = m_score + 1;
+    loadGif();
   } 
-  else if(gif.domain == "imgur.com") {
-    if($.isEmptyObject(gif.media_embed)) {
-      load_gif();
-    } else {
-      decoded = $('<div/>').html(gif["media_embed"].content).text();
-      elem.innerHTML = decoded;
-    }
-  } else {
-    url = gif.url;
-    elem.innerHTML = '<img src="' + url + '" />';
+  else if(m_skip && f_accept) {
+    f_score = f_score + 1;  
+    loadGif();
   }
+  else if(m_skip && f_skip) {
+    loadGif();
+  }
+  else if(f_accept && m_accept) {
+    m_score = m_score + 1;
+    f_score = f_score + 1;
+    loadGif();
+  }
+  
+  document.getElementById('f_player_score').innerText = f_score;
+  document.getElementById('m_player_score').innerText = m_score;
 }
 
-function end_game() {
+function endGame() {
   document.getElementById('game_container').style.display = 'none';
   document.getElementById('game_winner').style.display = 'block';
 
-  if(f_score == max) {
-    document.getElementById('player_winner').innerHTML = f_name + ' wins!';
-  } else {
+  if(f_score < m_score) {
     document.getElementById('player_winner').innerHTML = m_name + ' wins!';
   }
+  else if(f_score > m_score) {
+    document.getElementById('player_winner').innerHTML = f_name + ' wins!';
+  }
+  else {
+    document.getElementById('player_winner').innerHTML = 'Draw! Good job!';
+  }  
 }
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function unavailable() {
-  load_gif();
+function newGame() {
+  document.getElementById('game_winner').style.display = 'none';
+
+  document.getElementById('female_input').value = f_name;
+  document.getElementById('male_input').value = m_name;
+
+  document.getElementById('f_player_score').innerText = "0";
+  document.getElementById('m_player_score').innerText = "0";
+
+  document.getElementById('game_start').style.display = 'block';
 }
