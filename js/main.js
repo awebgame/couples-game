5b5a4bf1b084f9862dd18b45d43ba8fd8f73b332
awebgame <32238655+awebgame@users.noreply.github.com>
2017-09-27 10:11:54 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -18,10 +18,8 @@ var token = "";
 
 var api_url = "https://oauth.reddit.com/r/nsfw_gifs/search.json?restrict_sr=on&include_over_18=on&sort=relevance&t=all&limit=50";
 
-f_tags = ["dildo", "blowjob", "finger", "footjob", "ass", "fuck+pussy", "69", "cowgirl", "boobs", "suck+cock", "spread", "tit+fuck", "doggy"];
-m_tags = ["lick+pussy", "lick+ass", "doggy", "fuck+pussy", "69", "spank"];
-
-a_tags = ["anal"];
+f_tags = ["dildo", "blowjob", "finger", "footjob", "ass", "69", "cowgirl", "boobs", "suck+cock", "spread", "tit+fuck", "doggy", "position", "anal", "couple"];
+m_tags = ["lick+pussy", "lick+ass", "doggy", "fuck+pussy", "69", "spank", "finger+ass", "position", "anal", "couple"];
 
 function guidGenerator() {
   var S4 = function() {
